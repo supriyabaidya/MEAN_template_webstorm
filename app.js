fdc1d3e14fc30b88e716502bd2fa276fb3deb66b
supriyabaidya <33324876+supriyabaidya@users.noreply.github.com>
2018-08-28 18:24:04 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var cors = require('cors');
+
+var config = require('./config');
+
+// var appRoutes = require('./routes/index');
+
+var app = express();
+
+mongoose.connect(config.database, (err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log('MongoDB Database connected successfully');
+    }
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+app.get('/', function (req, res) {
+    res.json({
+        message: '"Template server started @3000 !" And "MongoDB Database connected successfully"'
+    });
+    // res.send('"Template server started @3000 !" And "MongoDB Database connected successfully"');
+});
+
+// app.use('/', appRoutes);
+//
+// catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//     next(createError(404));
+// });
+
+app.listen(config.port, (err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log('Template server started @' + config.port);
+    }
+});
+
+module.exports = app;
