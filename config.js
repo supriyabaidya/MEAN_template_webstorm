1d4d2ca70edd0b37f629a7d6ed8f622ba5ca088f
supriyabaidya <33324876+supriyabaidya@users.noreply.github.com>
2018-08-31 23:45:46 +0530

    update config

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
     // database: 'mongodb://localhost:27017/test',
-    database: 'mongodb://supriyo63:sb9051568624@ds131041.mlab.com:31041/nodejs-mongo-test',
+    database: 'mongodb://user6BJ:NdPUH1wCPoWJMlkB@mongodb:27017/sampledb',
+    // database: 'mongodb://supriyo63:sb9051568624@ds131041.mlab.com:31041/nodejs-mongo-test',
+    // database: 'mongodb://supriyo63:sb9051568624@ds131041.mlab.com:31041/nodejs-mongo-test',
     port: 3030
 };
